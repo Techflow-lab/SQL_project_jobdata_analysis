5a8db6f598a9f28b85e540724eb5a154397a688c
Techflow-lab <zhuodali028@gmail.com>
2025-10-18 21:45:12 +0200

    Update 1_top_paying_jobs.sql

--- 1_top_paying_jobs.sql
+++ 1_top_paying_jobs.sql
@@ -1,7 +1,6 @@
 -- Topic: What are the top-paying data analyst jobs
 
 SELECT	
-	job_id,
 	job_title,
 	job_location,
 	job_schedule_type,
@@ -14,7 +13,8 @@ LEFT JOIN company_dim ON job_postings_fact.company_id = company_dim.company_id
 WHERE
     job_title_short = 'Data Analyst' AND 
     job_location = 'Anywhere' AND 
-    salary_year_avg IS NOT NULL
+    salary_year_avg IS NOT NULL AND
+	job_work_from_home = True
 ORDER BY
     salary_year_avg DESC
-LIMIT 10;
+LIMIT 20;
